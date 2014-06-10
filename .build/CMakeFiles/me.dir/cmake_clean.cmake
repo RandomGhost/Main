03f9a851f748dd2014a4be3289709a279333c28e
Emile Cadorel <ecadorel@sqli.com>
2014-06-10 17:25:46 +0200

    blabla

---
+++ .build/CMakeFiles/me.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/me.dir/main.cc.o"
+  "me.pdb"
+  "me"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/me.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
