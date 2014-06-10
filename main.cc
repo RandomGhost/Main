932e94184643daf98228f1a3dfaa8ea2a9ecc959
Emile Cadorel <ecadorel@sqli.com>
2014-06-10 17:29:17 +0200

    blabla

--- main.cc
+++ main.cc
@@ -1,9 +1,10 @@
 #include <cstdlib>
-
+#include <unistd.h>
+#include <stdio.h>
 
 int main() {
   while ( 1 ) {
-    system ( "./push.sh" );
+    system ( "./../push.sh" );
     sleep ( 1 );
   }
 }
